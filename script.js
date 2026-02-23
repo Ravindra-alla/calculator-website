65d5bd91876cfe1f159f38069066114a9a011fd1
Alla Naga Ravindra <allanagaravindra@gmail.com>
2026-02-23 15:52:56 +0530

    Initial calculator app created

---
+++ script.js
@@ -0,0 +1,7 @@
+function add() {
+  let n1 = Number(document.getElementById("num1").value);
+  let n2 = Number(document.getElementById("num2").value);
+
+  document.getElementById("result").innerText =
+    "Result: " + (n1 + n2);
+}
