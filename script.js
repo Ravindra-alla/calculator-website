a1ae53ec92ffb3fcf1f5b62d5a05d61f5707e79f
Alla Naga Ravindra <allanagaravindra@gmail.com>
2026-02-23 20:22:15 +0530

    demo project

--- script.js
+++
@@ -1,7 +0,0 @@
-function subtract() {
-  let n1 = Number(document.getElementById("num1").value);
-  let n2 = Number(document.getElementById("num2").value);
-
-  document.getElementById("result").innerText =
-    "Result: " + (n1 - n2);
-}
